76d6e16eb785a21c07acc301e8e35c2091492bf9
Mahdi <bitsgenix@gmail.com>
2024-11-26 11:15:35 +0330

    feat(book): The structure of the project as well as the headings and files of the project are completely and accurately deleted and the project is formed again and again in a more principled and standard way. Also, files were added as documents in the /docs directory for participation, as well as FAQ for frequently asked questions.

--- Basic Operations/Traversal/Traversal.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-     int arr[2]={1,4};
-     int size = sizeof(arr) / sizeof(arr[0]);
-     for(int i =0;i<size;i++){
-        printf("%d\n", arr[i]);
-     }
-	return 0;
-}
--- MemorySegmentation/Bounds Checking/index_checking_example.py
+++
@@ -1,9 +0,0 @@
-def access_array(arr, index):
-    try:
-        print(f"Value at index {index}: {arr[index]}")
-    except IndexError:
-        print("Error: Index out of bounds.")
-
-if __name__ == "__main__":
-    my_array = [1, 2, 3, 4, 5]
-    access_array(my_array, 5)  # This should trigger the IndexError
