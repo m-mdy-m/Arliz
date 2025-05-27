cde70ea5e1c5407cbad4ec34a8f32b2f5cfb1601
mahdi <bitsgenix@gmail.com>
2025-05-27 14:16:29 +0330

    refactor(book): entire structure of the book changed. Various parts were added, from the history of computing and computers to how they work and how to create presentations, the history of presentations, and so on.

--- Exercises/Chapter-1/1/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int array_size;
-    printf("Enter size of your array: ");
-    scanf("%d", &array_size);
-    int total_memory = array_size * 4;
-    printf("Total memory required = %d bytes\n", total_memory);
-    return 0;
-}
